b965b58ae164d6da0c9fc70c0d3b71b7d2e01999
Koen Griffioen <koen.griffioen@capgemini.com>
2020-06-25 10:04:02 +0200

    Initial commit

---
+++ src/main/java/com/something/something/cidemo/CidemoApplication.java
@@ -0,0 +1,13 @@
+package com.something.something.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidemoApplication.class, args);
+	}
+
+}
